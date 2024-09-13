a834fc61182fe23b0a569d5205a6ae2b3eec8a8e
Prateek saini <157352079+Prateek0000007@users.noreply.github.com>
2024-09-13 10:46:02 +0530

    HelloWorld.java

--- GFG.java
+++ GFG.java
@@ -1,27 +1,8 @@
-// Java program to take an integer
-// as input and print it
-import java.io.*;
-import java.util.Scanner;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-// Driver Class
-class GFG{
-	// main function
-	public static void main(String[] args)
-	{
-		// Declare the variables
-		int num;
-
-		// Input the integer
-		System.out.println("Enter the integer: ");
-
-		// Create Scanner object
-		Scanner s = new Scanner(System.in);
-
-		// Read the next integer from the screen
-		num = s.nextInt();
-
-		// Display the integer
-		System.out.println("Entered integer is: "
-						+ num);
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
 }
